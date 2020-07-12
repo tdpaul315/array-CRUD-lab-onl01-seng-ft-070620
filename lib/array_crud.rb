41bb104e58da25fe4446d74abfdf1d01c9d0d25a
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-12 22:44:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << 
+  create_an_array << "my family"
 end
 
 def add_element_to_start_of_array(array, element)
