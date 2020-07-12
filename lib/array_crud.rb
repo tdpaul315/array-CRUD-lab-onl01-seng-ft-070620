23d31064d2406ea045000a63374de34fbcbbe848
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-12 23:17:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array ([create_an_array, " "]) << "arrays!"
+  add_element_to_end_of_array ([create_an_array, "marliee"]) << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
